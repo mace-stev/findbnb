3a3667337538403c27a4e778fcc74310a615f859
Elliot <elliot.winton@gmail.com>
2025-02-04 20:26:02 -0500

    create spot images done

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -8,7 +8,10 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Spot.hasMany(models.SpotImage, {
+        foreignKey: "spotId",
+        onDelete: "CASCADE",
+      });
     }
   }
   Spot.init(
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,63 +1,92 @@
-const express = require('express');
+const express = require("express");
 
-
-const { restoreUser, requireAuth } = require('../../utils/auth');
-const { Spot } = require('../../db/models');
+const { restoreUser, requireAuth } = require("../../utils/auth");
+const { Spot, SpotImage } = require("../../db/models");
 
 const router = express.Router();
 
-router.get('/', async(req, res)=>{
-    const allSpots= await Spot.findAll()
-    res.json(allSpots)
-})
+router.get("/", async (req, res) => {
+  const allSpots = await Spot.findAll();
+  res.json(allSpots);
+});
 
+router.get("/current", async (req, res) => {
+  const oneSpot = await Spot.findAll({
+    where: {
+      ownerId: req.user.id,
+    },
+  });
+  res.json(oneSpot);
+});
 
-router.get('/:spotId', async(req, res)=>{
-    const oneSpot= await Spot.findOne({
-        where:{
-            id: req.params.spotId
-        }
-    })
-    res.json(oneSpot)
-})
+router.get("/:spotId", async (req, res) => {
+  const oneSpot = await Spot.findOne({
+    where: {
+      id: req.params.spotId,
+    },
+  });
+  res.json(oneSpot);
+});
 
-router.get('/current', async(req, res)=>{
-    const oneSpot= await Spot.findAll({
-        where:{
-            ownerId: req.user.id
-        }
-    })
-    res.json(oneSpot)
-})
-router.post(
-    '/',
-    async (req, res) => {
-        const { address, city, state, country, lat, lng, name, description, price } = req.body;
-        const ownerId= req.user.id
-        
-    
-        const spot = await Spot.create({ ownerId, address, city, state, country, lat, lng, name, description, price });
+router.post("/", async (req, res) => {
+  const { address, city, state, country, lat, lng, name, description, price } =
+    req.body;
+  const ownerId = req.user.id;
 
+  const spot = await Spot.create({
+    ownerId,
+    address,
+    city,
+    state,
+    country,
+    lat,
+    lng,
+    name,
+    description,
+    price,
+  });
 
+  return res.json({
+    id: spot.id,
+    ownerId: ownerId,
+    address: address,
+    city: city,
+    state: state,
+    country: country,
+    lat: lat,
+    lng: lng,
+    name: name,
+    description: description,
+    price: price,
+    createdAt: spot.createdAt,
+    updatedAt: spot.updatedAt,
+  });
+});
 
-       return res.json({
-            id: spot.id,
-            ownerId: ownerId,
-            address: address,
-            city: city,
-            state: state,
-            country: country,
-            lat: lat,
-            lng: lng,
-            name: name,
-            description: description,
-            price: price,
-            createdAt: spot.createdAt,
-            updatedAt: spot.updatedAt
-        })
+router.post("/:spotId/images", requireAuth, async (req, res) => {
+  const { spotId } = req.params;
+  const { url, preview } = req.body;
 
-        
-    }
-);
+  const spot = await Spot.findByPk(spotId);
+  if (!spot) {
+    return res.status(404).json({ message: "Spot couldn't be found" });
+  }
+
+  if (spot.ownerId !== req.user.id) {
+    return res.status(403).json({ message: "Forbidden" });
+  }
+
+  const newImage = await SpotImage.create({
+    spotId,
+    url,
+    preview,
+  });
+
+  return res.status(200).json({
+    id: newImage.id,
+    url: newImage.url,
+    preview: newImage.preview,
+  });
+});
 
 module.exports = router;
