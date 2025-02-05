f42df8d3257aa53717558949717a58a5968f5583
Elliot <elliot.winton@gmail.com>
2025-02-04 19:31:50 -0500

    spot name validation fixed

--- backend/db/models/spot.js
+++ backend/db/models/spot.js
@@ -1,7 +1,5 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
+"use strict";
+const { Model } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Spot extends Model {
     /**
@@ -13,59 +11,62 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  Spot.init({
-    ownerId: {
-      type: DataTypes.INTEGER
+  Spot.init(
+    {
+      ownerId: {
+        type: DataTypes.INTEGER,
+      },
+      address: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      city: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      state: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      country: {
+        allowNull: false,
+        type: DataTypes.STRING,
+      },
+      lat: {
+        type: DataTypes.DECIMAL,
+        validate: {
+          min: -90,
+          max: 90,
+        },
+      },
+      lng: {
+        type: DataTypes.DECIMAL,
+        validate: {
+          min: -180,
+          max: 180,
+        },
+      },
+      name: {
+        type: DataTypes.STRING,
+        validate: {
+          len: [0, 50],
+        },
+      },
+      description: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+      },
+      price: {
+        type: DataTypes.DECIMAL,
+        validate: {
+          min: 0,
+        },
+      },
     },
-    address: {
-      allowNull: false,
-       type: DataTypes.STRING
-    },
-    city: {
-      allowNull: false,
-      type: DataTypes.STRING
-    },
-    state: {
-      allowNull: false,
-      type: DataTypes.STRING
-    },
-    country: {
-      allowNull: false,
-      type: DataTypes.STRING
-    },
-    lat: {
-      type: DataTypes.DECIMAL,
-      validate: {
-        min: -90,
-        max: 90
-      }
-    },
-    lng: {
-      type: DataTypes.DECIMAL,
-      validate: {
-        min: -180,
-        max: 180
-      }
-    },
-    name: {
-      type: DataTypes.STRING,
-      validate: {
-        max: 50
-      }
-    },
-    description: {
-      allowNull: false,
-      type: DataTypes.TEXT
-    },
-    price: {
-      type: DataTypes.DECIMAL,
-      validate: {
-        min: 0
-      }
-      }
-  }, {
-    sequelize,
-    modelName: 'Spot',
-  });
+    {
+      sequelize,
+      modelName: "Spot",
+    }
+  );
   return Spot;
 };
