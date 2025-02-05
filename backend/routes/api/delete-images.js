70e01aee1ad417a61389d653ae50284c33277153
Elliot <elliot.winton@gmail.com>
2025-02-05 00:12:21 -0500

    delete spot and delete image for a spot done

---
+++ backend/routes/api/delete-images.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+const { restoreUser, requireAuth } = require("../../utils/auth");
+const { Spot, SpotImage } = require("../../db/models");
+const { EmptyResultError } = require("sequelize");
+
+const router = express.Router();
+
+router.delete("/spot-images/:imageId", requireAuth, async (req, res, next) => {
+  const { imageId } = req.params;
+  const image = await SpotImage.findByPk(imageId);
+  if (!image) {
+    return res.status(404).json({ message: "Image couldn't be found" });
+  }
+
+  const spot = await Spot.findByPk(image.spotId);
+  if (spot.ownerId !== req.user.id) {
+    return res
+      .status(403)
+      .json({ message: "Forbidden" });
+  }
+  try {
+    await image.destroy();
+    return res.status(200).json({ message: "Successfully deleted" });
+  } catch (e) {
+    next(e);
+  }
+});
+
+module.exports = router;
--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -2,85 +2,133 @@ const express = require("express");
 
 const { restoreUser, requireAuth } = require("../../utils/auth");
 const { Spot, SpotImage } = require("../../db/models");
+const { EmptyResultError } = require("sequelize");
 
 const router = express.Router();
 
+router.get("/", async (req, res, next) => {
+  try {
+    const allSpots = await Spot.findAll();
+    res.json(allSpots);
+  } catch (e) {
+    next(e);
+  }
+});
 
-router.get('/', async(req, res)=>{
-    try{
-        
-    const allSpots= await Spot.findAll()
-    res.json(allSpots)
-    catch(e){
-        next(e);    
+router.get("/current", async (req, res, next) => {
+  try {
+    console.log(req.user.id);
+    if (!req.user.id) {
+      const userError = new Error("User must be signed in");
+      userError.status = 403;
+      throw userError;
     }
-})
-router.get('/current', async(req, res)=>{
-    try{
-        
-    console.log(req.user.id)
-        if(!req.user.id){
-            const userError = new Error("User must be signed in")
-            userError.status = 403;
-            throw userError;
-            
-        }
-    const oneSpot= await Spot.findAll({
-        where:{
-            ownerId: req.user.id
-        }
-    })
-    res.json(oneSpot)
-    catch(e){
-        next(e);
-        
+    const oneSpot = await Spot.findAll({
+      where: {
+        ownerId: req.user.id,
+      },
+    });
+    res.json(oneSpot);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get("/:spotId", async (req, res, next) => {
+  try {
+    const oneSpot = await Spot.findOne({
+      where: {
+        id: req.params.spotId,
+      },
+    });
+    res.json(oneSpot);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.post("/", async (req, res, next) => {
+  try {
+    const {
+      address,
+      city,
+      state,
+      country,
+      lat,
+      lng,
+      name,
+      description,
+      price,
+    } = req.body;
+    const ownerId = req.user.id;
+
+    const spot = await Spot.create({
+      ownerId,
+      address,
+      city,
+      state,
+      country,
+      lat,
+      lng,
+      name,
+      description,
+      price,
+    });
+
+    return res.json({
+      id: spot.id,
+      ownerId: ownerId,
+      address: address,
+      city: city,
+      state: state,
+      country: country,
+      lat: lat,
+      lng: lng,
+      name: name,
+      description: description,
+      price: price,
+      createdAt: spot.createdAt,
+      updatedAt: spot.updatedAt,
+    });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.post("/:spotId/images", requireAuth, async (req, res, next) => {
+  try {
+    const { spotId } = req.params;
+    const { url, preview } = req.body;
+
+    const spot = await Spot.findByPk(spotId);
+    if (!spot) {
+      return res.status(404).json({ message: "Spot couldn't be found" });
     }
-})
 
-router.get('/:spotId', async(req, res)=>{
-    const oneSpot= await Spot.findOne({
-        where:{
-            id: req.params.spotId
-        }
-    })
-    res.json(oneSpot)
-})
-
-
-router.post(
-    '/',
-    async (req, res) => {
-        const { address, city, state, country, lat, lng, name, description, price } = req.body;
-        const ownerId= req.user.id
-        
-    
-        const spot = await Spot.create({ ownerId, address, city, state, country, lat, lng, name, description, price });
-
-
-
-       return res.json({
-            id: spot.id,
-            ownerId: ownerId,
-            address: address,
-            city: city,
-            state: state,
-            country: country,
-            lat: lat,
-            lng: lng,
-            name: name,
-            description: description,
-            price: price,
-            createdAt: spot.createdAt,
-            updatedAt: spot.updatedAt
-        })
-
-        
+    if (spot.ownerId !== req.user.id) {
+      return res.status(403).json({ message: "Forbidden" });
     }
-);
-  
-  router.post("/:spotId/images", requireAuth, async (req, res) => {
+
+    const newImage = await SpotImage.create({
+      spotId,
+      url,
+      preview,
+    });
+
+    return res.status(200).json({
+      id: newImage.id,
+      url: newImage.url,
+      preview: newImage.preview,
+    });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.put("/:spotId", requireAuth, async (req, res, next) => {
   const { spotId } = req.params;
-  const { url, preview } = req.body;
+  const { address, city, state, country, lat, lng, name, description, price } =
+    req.body;
 
   const spot = await Spot.findByPk(spotId);
   if (!spot) {
@@ -91,17 +139,55 @@ router.post(
     return res.status(403).json({ message: "Forbidden" });
   }
 
-  const newImage = await SpotImage.create({
-    spotId,
-    url,
-    preview,
-  });
+  try {
+    await spot.update({
+      address,
+      city,
+      state,
+      country,
+      lat,
+      lng,
+      name,
+      description,
+      price,
+    });
 
-  return res.status(200).json({
-    id: newImage.id,
-    url: newImage.url,
-    preview: newImage.preview,
-  });
+    return res.status(200).json({
+      id: spot.id,
+      ownerId: spot.ownerId,
+      address: spot.address,
+      city: spot.city,
+      state: spot.state,
+      country: spot.country,
+      lat: spot.lat,
+      lng: spot.lng,
+      name: spot.name,
+      description: spot.description,
+      price: spot.price,
+      createdAt: spot.createdAt,
+      updatedAt: spot.updatedAt,
+    });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.delete("/:spotId", requireAuth, async (req, res, next) => {
+  const { spotId } = req.params;
+  const spot = await Spot.findByPk(spotId);
+  if (!spot) {
+    return res.status(404).json({ message: "Spot couldn't be found" });
+  }
+
+  if (spot.ownerId !== req.user.id) {
+    return res.status(403).json({ message: "Forbidden" });
+  }
+  try {
+    await spot.destroy();
+    return res.status(200).json({ message: "Successfully deleted" });
+  } catch (e) {
+    next(e);
+  }
 });
 
 module.exports = router;
