98fafad2eb54e8f31d72695456ae4d1a6da0e4f5
Cathy-45 <catherine.sichone@gmail.com>
2025-02-06 20:36:21 -0600

    added reviewImages file

--- backend/db/seeders/5-reviewImages.js
+++ backend/db/seeders/5-reviewImages.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const { ReviewImage} = require('../models');
-
+const { Booking } = require('../models');
+const bcrypt = require("bcryptjs");
 
 let options = {};
 if (process.env.NODE_ENV === 'production') {
@@ -10,12 +10,34 @@ if (process.env.NODE_ENV === 'production') {
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await ReviewImage.bulkCreate([
-      //seeder details here
-    ], { validate: true });
-  },
+    await queryInterface.bulkInsert('ReviewImages', [
+      {
+        id: 1,
+        reviewId: 1,
+        Url: 'https://example.com/image1.jpg',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    },
+    {
+        id: 2,
+        reviewId: 1,
+        Url: 'https://example.com/image2.jpg',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    },
+    {
+        id: 3,
+        reviewId: 2,
+        Url: 'https://example.com/image3.jpg',
+        createdAt: new Date(),
+        updatedAt: new Date(),
+    },
+], options);
+},
 
-  async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete('ReviewImages', null, {});
+async down(queryInterface, Sequelize) {
+  return queryInterface.bulkDelete('ReviewImages', null, options);
   }
 };
+
+
