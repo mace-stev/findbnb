65bc422bc877805a4bc0905fbdb81690900ab001
Cathy-45 <catherine.sichone@gmail.com>
2025-03-08 23:35:00 -0600

    Completed Frontend setup phase-0

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,10 +4,18 @@ import { Provider } from 'react-redux';
 import App from './App';
 import './index.css';
 import configureStore from './store/store';
+import { restoreCSRF, csrfFetch } from './store/csrf';
 
 const store = configureStore();
 store.dispatch({type: 'hello'});
-console.log(store.getState())
+
+if (import.meta.env.MODE !== 'production') {
+  restoreCSRF();
+
+  window.csrfFetch = csrfFetch;
+  window.store = store;
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
