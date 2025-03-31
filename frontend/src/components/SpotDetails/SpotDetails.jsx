cba09cd42463cd3b2764c4ab6a99c5deb8ebcaf9
mace-stev <williamstevenson107@gmail.com>
2025-03-31 18:40:51 -0500

    some last minute changes

--- frontend/src/components/SpotDetails/SpotDetails.jsx
+++ frontend/src/components/SpotDetails/SpotDetails.jsx
@@ -36,7 +36,7 @@ function SpotDetails() {
 
     return (<section className="body-section">
         {Object.values(oneSpot)?.map((element) => {
-            return (<React.Fragment key={element.id}>
+            return (<div key={element.id}>
                 <div>
                     <h1 className="spot-details-title">{element?.name}</h1>
                     <h3>{`${element?.city}, ${element?.state}, ${element?.country}`}</h3>
@@ -51,55 +51,71 @@ function SpotDetails() {
                     </div>
                     <div className="spot-details-secondary-image-container">
                         {element.SpotImages?.[1]?.url ? (
-                            <img className="spotImage" src={element.SpotImages[1].url} />
+                            <img className="spotImage" id="spotImage1" src={element.SpotImages[1].url} />
                         ) : (
-                            <FaImage className="spotImage" color="gray" />
+                            <FaImage className="spotImage" id="spotImage1" color="gray" />
                         )}
                         {element?.SpotImages?.[2]?.url ? (
-                            <img className="spotImage" src={element?.SpotImages?.[2]?.url} />
+                            <img className="spotImage" id="spotImage2" src={element?.SpotImages?.[2]?.url} />
                         ) : (
-                            <FaImage className="spotImage" color="gray" />
+                            <FaImage className="spotImage" id="spotImage2" color="gray" />
                         )}
                         {element?.SpotImages?.[3]?.url ? (
-                            <img className="spotImage" src={element?.SpotImages?.[3]?.url} />
+                            <img className="spotImage" id="spotImage3"src={element?.SpotImages?.[3]?.url} />
                         ) : (
-                            <FaImage className="spotImage" color="gray" />
+                            <FaImage className="spotImage" id="spotImage3" color="gray" />
                         )}
                         {element?.SpotImages?.[4]?.url ? (
-                            <img className="spotImage" src={element?.SpotImages?.[4]?.url} />
+                            <img className="spotImage" id="spotImage4"src={element?.SpotImages?.[4]?.url} />
                         ) : (
-                            <FaImage className="spotImage" color="gray" />
+                            <FaImage className="spotImage" id="spotImage4" color="gray" />
                         )}
                     </div>
                 </div>
-                <div className="spot-details-desciption">
-                    <h2 className="spot-details-host">{`Hosted by ${element?.Owner?.firstName} ${element?.Owner?.lastName}`}</h2>
-                    <p>{`${element?.description}`}</p>
-                </div>
-                <div className="spot-details-review-header-container">
-                    <FaStar className='star'/>
-                    <h3>{element?.avgStarRating?.toFixed(1)}</h3>
-                    <h3 className="reviews-num">{`${spotReviews.length} reviews`}</h3>
-                </div>
-                {currUser && currUser.id !== element.ownerId && (
-                    <button className="spot-button"><OpenModalMenuItem
-                        itemText="Post Your Review"
-                        onItemClick={() => {
-                            return (<div className="menu-overlay"></div>)
-                        }}
-                        onModalClose={() => {
-                            dispatch(fetchSpot(id))
-                            dispatch(fetchSpotReviews(id))
-                        }}
-                        modalComponent={<NewReview spotId={element.id} userId={currUser.id} />}
+                <div className="spot-details-description-container">
+                    <div className="spot-details-description">
+                        <h2 className="spot-details-host">{`Hosted by ${element?.Owner?.firstName} ${element?.Owner?.lastName}`}</h2>
+                        <p>{`${element?.description}`}</p>
+                
+                    <div className="spot-details-review-header-container">
+                        <FaStar className='star' />
+                        <h3>{element?.avgStarRating?.toFixed(1)}</h3>
+                        <h3 className="reviews-num">{`${spotReviews.length} reviews`}</h3>
+                    </div>
+                    {currUser && currUser.id !== element.ownerId && (
+                        <button className="spot-button"><OpenModalMenuItem
+                            itemText="Post Your Review"
+                            onItemClick={() => {
+                                return (<div className="menu-overlay"></div>)
+                            }}
+                            onModalClose={() => {
+                                dispatch(fetchSpot(id))
+                                dispatch(fetchSpotReviews(id))
+                            }}
+                            modalComponent={<NewReview spotId={element.id} userId={currUser.id} />}
 
-                    /></button>
-                )}
+                        /></button>
+                    )}
+                     </div>
+                    <div className="reserve-button-container">
+                        <div className="reserve-button-container-top">
+                            <p className="price-element">{<span className="spot-price-span">{`$${element?.price}`}</span>} night</p>
+                            <div className="spot-details-review-header-container" id="reserve-button-header-section">
+                                <FaStar className='star' />
+                                <h3>{element?.avgStarRating?.toFixed(1)}</h3>
+                                <h3 className="reviews-num">{`${spotReviews.length} reviews`}</h3>
+                            </div>
+                        </div>
+                        <button onClick={()=>{
+                            return alert("Feature Coming Soon...")
+                        }} className='reserve-button'>Reserve</button>
+                    </div>
+                </div>
 
-            </React.Fragment>)
+            </div>)
         })}
         {Object.values(spotReviews).map((element) => {
-            return (<React.Fragment key={element.id}>
+            return (<div key={element.id}>
                 <div className="review">
                     <h4>{element?.User?.firstName || element?.firstName}</h4>
                     <h4 className="review-date">{`${element?.createdAt.slice(5, 7)} ${element.createdAt.slice(0, 4)}`}</h4>
@@ -107,21 +123,21 @@ function SpotDetails() {
                 </div>
                 {currUser && currUser.id === element.userId && (
                     <button className="spot-button">
-                    <OpenModalMenuItem
-                    itemText="Delete"
-                    onItemClick={()=>{
-                        return(<div className="menu-overlay"></div>)
-                    }}
-                    modalComponent={<DeleteReview reviewId={element.id} spotId={id}/>}
-                    onModalClose={() => {
-                        dispatch(fetchSpot(id))
-                        dispatch(fetchSpotReviews(id))
-                    }}
-                />
-                </button>
+                        <OpenModalMenuItem
+                            itemText="Delete"
+                            onItemClick={() => {
+                                return (<div className="menu-overlay"></div>)
+                            }}
+                            modalComponent={<DeleteReview reviewId={element.id} spotId={id} />}
+                            onModalClose={() => {
+                                dispatch(fetchSpot(id))
+                                dispatch(fetchSpotReviews(id))
+                            }}
+                        />
+                    </button>
                 )}
 
-            </React.Fragment>)
+            </div>)
         })}
     </section>)
 }
